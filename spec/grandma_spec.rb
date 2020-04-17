f2014b6342cdac22856e0d21c25300406ef3a3ef
adeeba reaz <adeebarivera@outlook.com>
2020-04-17 14:38:20 +0000

    Automatically backed up by Learn

--- spec/grandma_spec.rb
+++ spec/grandma_spec.rb
@@ -20,3 +20,20 @@ describe '#speak_to_grandma' do
   end
 
 end
+    
+    
+  end
+
+
+def speak_to_grandma(phrase)
+  if phrase =="I LOVE YOU GRANDMA!"
+    return'I LOVE YOU TOO PUMPKIN!'
+  elsif phrase != phrase.upcase
+    return "HUH?! SPEAK UP, SONNY!"
+  end
+  'NO, NOT SINCE 1938!'
+end
+
+
+speak_to_grandma ("test")
+  
